41fb5713bf96e70b84adce72bfdbae34fc8ca476
reval59 <modesty723@gmail.com>
2020-09-15 23:33:24 +0800

    init dist subtree

--- src/plugins/d3.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import * as d3 from 'd3'
-import d3Tip from "d3-tip";
-
-d3.tip = d3Tip;
-Vue.set(Vue.prototype, 'd3', d3);
--- src/plugins/lodash.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import _ from 'lodash'
-
-
-Vue.set(Vue.prototype, 'lodash', _);
